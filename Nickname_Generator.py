78f65b4ab480aa865405fea1d836e399f501bc55
K4kazito <166251083+K4kazito@users.noreply.github.com>
2024-08-17 23:05:30 -0300

    Add files via upload

---
+++ Nickname_Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to our nickname generator!")
+X =input("What is the first letter of your name?")
+Y =input("What is your favorite color?")
+print("Your new nickname is: " + Y +" " + X)
